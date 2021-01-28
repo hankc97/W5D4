bd246ed80bcb1e805d0c01a26b6cc6e644f9f58b
Lane Harris <lrharris215@gmail.com>
2021-01-28 14:33:26 -0500

    finish Course has_many and has_one methods

--- Associations_exercise/app/models/course.rb
+++ Associations_exercise/app/models/course.rb
@@ -10,4 +10,23 @@
 #  updated_at    :datetime         not null
 #
 class Course < ApplicationRecord
+
+        has_many :enrollments,
+        primary_key: :id,
+        foreign_key: :course_id,
+        class_name: :Enrollment
+
+        has_many :enrolled_students,
+        through: :enrollments,
+        source: :user
+
+        has_one :prerequisite,
+        primary_key: :prereq_id,
+        foreign_key: :id,
+        class_name: :Course
+
+        has_one :instructor,
+        primary_key: :instructor_id,
+        foreign_key: :id,
+        class_name: :User
 end
